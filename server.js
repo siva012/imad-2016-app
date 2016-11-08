82342d69d20ad34d8fe4db7683cd6d7cb47edf28
siva012 <sivaram1296@gmail.com>
2016-11-09 01:17:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles = {
+  'articles-one':{
+      title : 'One' ,
+      content : `<p>This is a demo article
+                 </p>`
+  },
+  'artiles-two':{
+      title : 'two' ,
+      content : `<p>This is a demo article
+                 </p>`
+      
+  },
+  'articles-three':{
+      title : 'three' ,
+      content : `<p>This is a demo article
+                 </p>`
+      
+  }
+    
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var content = data.content;
+    
+    var htmlContent= `<html>
+                        <head>
+                            <title>${title}</title>
+                        </head>
+                        <body>
+                            ${content}
+                        </body>
+                    </html>`;
+                return htmlContent;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
