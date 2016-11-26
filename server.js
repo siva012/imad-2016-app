7b043ea38674e6cec77e2dd21648e55f4b8f0653
siva012 <sivaram1296@gmail.com>
2016-11-26 22:16:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,15 +18,22 @@ app.use(morgan('combined'));
 var articles = {
   'register':{
       title : 'Register' ,
-      content : `<form class= "ui form">
-                    <div class="field">
-                        <label>Usernamee</label>
-                        <input type="text" name="uname" placeholder="User Name">
-                     </div>
-                     <div class="field">
+      content : `<form class="ui form">
+                      <div class="field">
+                        <label>First Name</label>
+                        <input type="text" name="first-name" placeholder="First Name">
+                      </div>
+                      <div class="field">
                         <label>Last Name</label>
-                        <input type="password" name="pass" placeholder="Password">
-                    </div>
+                        <input type="text" name="last-name" placeholder="Last Name">
+                      </div>
+                      <div class="field">
+                        <div class="ui checkbox">
+                          <input type="checkbox" tabindex="0" class="hidden">
+                          <label>I agree to the Terms and Conditions</label>
+                        </div>
+                      </div>
+                      <button class="ui button" type="submit">Submit</button>
                 </form>`
   },
   'signin':{
